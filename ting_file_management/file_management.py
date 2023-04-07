0538ac5edc297cdb565dce8d88c2114de9fd0b18
Sally Diament <sally.barzilai@gmail.com>
2023-04-06 22:46:14 -0300

    feat: including file_management method

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    list_file = []
+    if not path_file.endswith(".txt"):
+        sys.stderr.write("Formato inválido")
+    try:
+        with open(path_file) as file:
+            for line in file:
+                new_line = line.replace('\n', '')
+                list_file.append(new_line)
+            return list_file
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
