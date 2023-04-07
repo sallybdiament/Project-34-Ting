e25ed401e4662307371d8052e7ff59d7bf520cf6
Sally Diament <sally.barzilai@gmail.com>
2023-04-06 23:16:43 -0300

    feat: file_process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,23 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for element in instance.data:
+       if element['nome_do_arquivo'] == path_file:
+           return None
+    content_file = txt_importer(path_file)
+    words_dict = {  
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(content_file),
+        'linhas_do_arquivo': content_file
+    }
+    instance.enqueue(words_dict)
+    print(words_dict, file=sys.stdout)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+   pass
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+   pass
--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,20 +3,20 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        self._data = list()
+        self.data = list()
 
     def __len__(self):
-        return len(self._data)
+        return len(self.data)
 
     def enqueue(self, value):
-        self._data.append(value)
+        self.data.append(value)
 
     def dequeue(self):
-        if len(self._data) == 0:
+        if len(self.data) == 0:
             return None
-        return self._data.pop(0)
+        return self.data.pop(0)
 
     def search(self, index):
-        if index < 0 or index > (len(self._data)-1):
+        if index < 0 or index > (len(self.data)-1):
             raise IndexError("Índice Inválido ou Inexistente")
-        return self._data[index]        
+        return self.data[index]        
