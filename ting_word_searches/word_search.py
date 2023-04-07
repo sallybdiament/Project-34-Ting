b32c6213b990f2dd1984ba3e122c53c3745ebebd
Sally Diament <sally.barzilai@gmail.com>
2023-04-07 16:58:43 -0300

    feat: implementing exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,20 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    found_list = list()
+    found_dict = dict()
+    found_dict["ocorrencias"] = list()
+    found_dict["arquivo"] = 0
+    for element in instance.data:
+        for index, linha in enumerate(element["linhas_do_arquivo"]):
+            if word.lower() in linha.lower():
+                if found_dict["arquivo"] == 0:
+                    found_dict["arquivo"] = element["nome_do_arquivo"]
+                    found_dict["palavra"] = word
+                    found_dict["ocorrencias"].append({"linha": index + 1})
+                    found_list.append(found_dict)
+                else:
+                    found_dict["ocorrencias"].append({"linha": index + 1})
+    return found_list
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    pass
